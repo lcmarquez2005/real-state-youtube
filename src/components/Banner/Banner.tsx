99eec01d3a8c9302b08c8812af45557c82cd6d86
lcmarquez2005 <93497992+lcmarquez2005@users.noreply.github.com>
2025-02-04 18:49:48 -0600

    primer banner funcionando

--- src/components/Banner/Banner.tsx
+++ src/components/Banner/Banner.tsx
@@ -1,5 +1,13 @@
+
+
 export function Banner() {
     return (
-        <p>Banner</p>
+        <div className="container relative mx-auto text-white">
+            <div className="pt-20 md:pt-0 min-h-[80vh] bg-banner bg-no-repeat bg-cover bg-center rounded-3xl relative flex flex-col items-center md:justify-center">
+                <h2 className="text-5xl font-semibold"> Descubre la vivienda ideal para tu familia en México</h2>
+                <p className="text-xl md:mt'8 mt-2">Con Abogados que te van a Asesorar en todo el papeleo</p>
+            </div>
+        </div>
+
     )
 }
