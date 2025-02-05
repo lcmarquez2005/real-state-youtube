ac8e6e2d98ae6f36a8cae28b19e74eebaaecedd3
lcmarquez2005 <93497992+lcmarquez2005@users.noreply.github.com>
2025-02-04 21:42:57 -0600

    Controls from floating serch

--- src/components/FloatedSearch/FloatedSearch.tsx
+++ src/components/FloatedSearch/FloatedSearch.tsx
@@ -8,7 +8,7 @@ import { Transition } from "../Transition" ;
 export function FloatedSearch() {
     return (
 
-        <Transition className="absolute bottom-10 md:-bottom-10 left-0 right-0 w-[75%] mx-auto">
+        <Transition className="absolute bottom-10 md:-bottom-10 left-0 right-0 w-[80%] lg:w-[60%] mx-auto">
 
             <div className="flex  flex-col justify-between gap-4 py-4 px-3 bg-white rounded-md md:flex rounded-md md:flex-row backdrop-blur shadow-light font-sans">
                 <SearchLocation />
--- src/components/SearchButton/SearchButton.tsx
+++ src/components/SearchButton/SearchButton.tsx
@@ -1,6 +1,10 @@
+import { RiSearch2Line } from 'react-icons/ri';
+
 
 export function SearchButton() {
     return (
-        <p>Search button</p>
+        <div className="gap-2 border-[1px] rounded-lg px-12 py-2 bg-secondary flex items-center text-white justify-center">
+            <RiSearch2Line className="text-2xl" />
+        </div>
     )
 }
