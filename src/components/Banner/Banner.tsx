017d3aefca9852048aa23a4438eafadf4ae89627
lcmarquez2005 <93497992+lcmarquez2005@users.noreply.github.com>
2025-02-04 20:11:41 -0600

    new banner and floated search control

--- src/components/Banner/Banner.tsx
+++ src/components/Banner/Banner.tsx
@@ -1,13 +1,17 @@
-
+import { FloatedSearch } from "../FloatedSearch"
 
 export function Banner() {
     return (
-        <div className="container relative mx-auto text-white">
+        <div className="container relative mx-auto">
             <div className="pt-20 md:pt-0 min-h-[80vh] bg-banner bg-no-repeat bg-cover bg-center rounded-3xl relative flex flex-col items-center md:justify-center">
-                <h2 className="text-5xl font-semibold"> Descubre la vivienda ideal para tu familia en México</h2>
-                <p className="text-xl md:mt'8 mt-2">Con Abogados que te van a Asesorar en todo el papeleo</p>
+                <div className="max-w-3xl text-center text-white">
+                    <h2 className="text-5xl font-semibold"> Descubre la vivienda ideal para tu familia en México</h2>
+                    <p className="text-xl md:mt8 mt-2">Con Abogados que se encargaran de la asesoria legal para tus necesidades y objetivos</p>
+                </div>
+                <FloatedSearch />
             </div>
         </div>
+
 
     )
 }
--- src/components/FloatedSearch/FloatedSearch.tsx
+++ src/components/FloatedSearch/FloatedSearch.tsx
@@ -1,6 +1,24 @@
+import { SearchLocation } from "../SearchLocation" ;
+import { SearchPriceRange } from "../SearchPriceRange" ;
+import { SearchProperty } from "../SearchProperty" ;
+import { SearchButton } from "../SearchButton" ;
+import { Transition } from "../Transition" ;
+
 
 export function FloatedSearch() {
     return (
-        <p>Floated search</p>
+
+        <Transition className="absolute bottom-10 md:-bottom-10 left-0 right-0 w-[75%] mx-auto">
+
+            <div className="flex flex-col justify-between gap-4 py-4 bg-white rounded-md md:flex rounded-md md:flex-row backdrop-blur shadow-light">
+                <SearchLocation />
+                <SearchProperty />
+                <SearchPriceRange />
+                <SearchButton />
+
+
+            </div>
+        
+        </Transition>
     )
 }
