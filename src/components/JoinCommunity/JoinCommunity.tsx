b6fed1ce68f53b330b05d8756054945afac26a18
lcmarquez2005 <93497992+lcmarquez2005@users.noreply.github.com>
2025-02-06 00:06:39 -0600

    change of design

--- src/components/JoinCommunity/JoinCommunity.tsx
+++ src/components/JoinCommunity/JoinCommunity.tsx
@@ -4,14 +4,16 @@ import { FaRegPaperPlane } from "react-icons/fa";
 export function JoinCommunity() {
     return (
 
-        <Transition className="grid items-center px-4 py-8 md:py-46 md:px-36 md:grid-cols-2 md:gap-4">
+        <Transition className="grid items-center px-4 py-8 md:py-44 md:px-36 md:grid-cols-2 md:gap-4 bg-secondary">
             <h3 className="max-w-lg text-4xl font-semibold text-white">
                 Contactanos para enterarte de las últimas novedades en el mercado inmobiliario
             </h3>
-            <button className="flex items-center justify-between py-5 text-white transition-all duration-100 bg-black rounded-lg px-7 m-fit hover:bg-black/70">
-                <FaRegPaperPlane />
-                <span className="ml-3">Contactanos</span>
-            </button>
+            <div className="mx-auto mt-5 md:mx-auto">
+                <button className="flex items-center justify-between py-5 text-white transition-all duration-100 bg-black rounded-lg px-7 w-fit hover:bg-black/70">
+                    <FaRegPaperPlane />
+                    <span className="ml-3">Contactanos</span>
+                </button>
+            </div>
         </Transition>
     )
 }
