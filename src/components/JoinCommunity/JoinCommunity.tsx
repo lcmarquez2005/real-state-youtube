edd72bca93a6d2b10b2797d245addbee1280c7e7
lcmarquez2005 <93497992+lcmarquez2005@users.noreply.github.com>
2025-02-05 23:45:45 -0600

    join to get our services page

--- src/components/JoinCommunity/JoinCommunity.tsx
+++ src/components/JoinCommunity/JoinCommunity.tsx
@@ -1,5 +1,17 @@
+import { Transition } from "../Transition";
+import { FaRegPaperPlane } from "react-icons/fa";
+
 export function JoinCommunity() {
     return (
-        <p>Join community</p>
+
+        <Transition className="grid items-center px-4 py-8 md:py-46 md:px-36 md:grid-cols-2 md:gap-4">
+            <h3 className="max-w-lg text-4xl font-semibold text-white">
+                Contactanos para enterarte de las últimas novedades en el mercado inmobiliario
+            </h3>
+            <button className="flex items-center justify-between py-5 text-white transition-all duration-100 bg-black rounded-lg px-7 m-fit hover:bg-black/70">
+                <FaRegPaperPlane />
+                <span className="ml-3">Contactanos</span>
+            </button>
+        </Transition>
     )
 }
