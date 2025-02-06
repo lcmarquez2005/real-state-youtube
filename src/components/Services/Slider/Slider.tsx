2eb611ae28943edd722e4df96cad45015586ce21
lcmarquez2005 <93497992+lcmarquez2005@users.noreply.github.com>
2025-02-05 20:50:54 -0600

    adding services to develop

--- src/components/Services/Slider/Slider.tsx
+++ src/components/Services/Slider/Slider.tsx
@@ -1,5 +1,37 @@
+import { Swiper, SwiperSlide } from 'swiper/react';
+import { Navigation, Pagination, Scrollbar } from 'swiper/modules';
+
+import 'swiper/css';
+import 'swiper/css/pagination';
+import 'swiper/css/scrollbar';
+import Image from "next/image";
+import { sliderDataImages } from './Slider.data';
+
 export function Slider() {
     return (
-        <p>Slide </p>
+        <Swiper
+                slidesPerView={2.5}
+                spaceBetween={20}
+                freeMode={true}
+                navigation
+                pagination={{ clickable: true }}
+                scrollbar={{ draggable: true }}
+                modules={[Navigation, Scrollbar]}
+                className='h-[240px] md:h-[300px] w-[300px] md:w-[500px]'
+                grabCursor
+            >
+                { sliderDataImages.map(({id, urlImage}) => (
+                    <SwiperSlide key={id}>
+                        <div className="relative w-[120px] md:w-auto w-full h-[180px] md:h-[280px]">
+                            <Image 
+                                src={`/assets/houses/${urlImage}`} 
+                                alt="House" 
+                                fill 
+                                className="rounded-xl object-cover"
+                            />
+                        </div>                    
+                    </SwiperSlide>
+                ))}
+        </Swiper>
     )
 }
