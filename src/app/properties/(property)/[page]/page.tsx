2234096101371860882d57486b9793f50ac4f2f4
lcmarquez2005 <93497992+lcmarquez2005@users.noreply.github.com>
2025-02-06 19:30:35 -0600

    initial structure to PropertiesPage

--- src/app/properties/(property)/[page]/page.tsx
+++ src/app/properties/(property)/[page]/page.tsx
@@ -1,10 +1,28 @@
 'use client'
 
+import { Error404 } from "@/components/Error404";
+import { Header } from "@/components/Header";
+import { dataProperties } from "@/components/Properties/Properties.data";
+import { Property } from "@/components/Property";
+import { TransitionPage } from "@/components/TransitionPage";
+import { useParams } from "next/navigation";
+
 export default function Page() {
+    const router = useParams()
+    const propertyId = router.page;
+    const filteredPage = dataProperties.find(property => property.id === Number(propertyId));
+    console.log(filteredPage);
+
 
     return (
         <>
-            <p>Page</p>
+            <TransitionPage />
+            <Header />
+            {filteredPage ? (
+                <Property />
+            ) : (
+                <Error404 />
+            )}
         </>
     )
 }
