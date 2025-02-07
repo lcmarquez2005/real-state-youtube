1829c390ad46aefa1003e0131d207dd878a01f46
lcmarquez2005 <93497992+lcmarquez2005@users.noreply.github.com>
2025-02-06 19:41:29 -0600

    404 not found error configured

--- src/components/Error404/Error404.tsx
+++ src/components/Error404/Error404.tsx
@@ -1,5 +1,17 @@
+import Image from 'next/image';
+import Link from 'next/link';
+
 export function Error404() {
     return (
-        <p>Error 404</p>
+        <div className="flex flex-col text-center items-center justify-center min-h-screen">
+            <h1 className="text-4xl font-bold text-gray-800">404 - Página no encontrada</h1>
+            <h2 className="text-3xl mb-5">AY! no encontre la pagina que estas buscando</h2>
+            <Image src="/assets/404-not-found.jpg" alt='Not Found' width={600} height={450} className='rounded-lg shadow-light'/>
+            <div className="text-center mt-5">
+                <Link href="/" className='px-3 py-2 bg-secondary text-white rounded-lg'>
+                    Regresar al inicio
+                </Link>
+            </div>
+        </div>
     )
 }
